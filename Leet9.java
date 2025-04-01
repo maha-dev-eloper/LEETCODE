c27d1920ca4e5c45a19d8918f43d7c36c8f25aa4
Mahadev Mallinath Shete <mahadevshete23@gmail.com>
2025-04-01 18:16:40 +0530

    leetcode 9

--- Leet9.java
+++ Leet9.java
@@ -1,5 +1,25 @@
 public class Leet9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
+        int original = x;
+        int reverseNumber = 0;
+        
+        while (x > 0) {
+            int d = x % 10;
+            reverseNumber = reverseNumber * 10 + d;
+            x = x / 10;
+        }
+
+        return reverseNumber == original;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Leet9 l = new Leet9();
+        int number = 121; 
+        boolean result = l.isPalindrome(number);
+        System.out.println(result);  
     }
 }
