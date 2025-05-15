eb9b586914ba6976f7ba0138ed14d674f603f412
Mahadev Mallinath Shete <mahadevshete23@gmail.com>
2025-05-15 22:53:16 +0530

    leetcode-2900

---
+++ leet2900.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class leet2900{
+  public List<String> getLongestSubsequence(String[] words, int[] groups) {
+    List<String> ans = new ArrayList<>();
+    int groupId = -1;
+
+    for (int i = 0; i < words.length; ++i) {
+      if (groups[i] != groupId) {
+        groupId = groups[i];
+        ans.add(words[i]);
+      }
+    }
+
+    return ans;
+  }
+}
