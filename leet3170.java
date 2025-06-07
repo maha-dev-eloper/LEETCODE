f97c5644f6447f10f98e2409da1109c55aaeefa6
Mahadev Mallinath Shete <mahadevshete23@gmail.com>
2025-06-07 22:16:45 +0530

    leetcode-3170

---
+++ leet3170.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class leet3170 {
+  public String clearStars(String s) {
+    StringBuilder sb = new StringBuilder(s);
+    List<Integer>[] buckets = new List[26];
+
+    for (int i = 0; i < 26; ++i)
+      buckets[i] = new ArrayList<>();
+
+    for (int i = 0; i < s.length(); ++i)
+      if (s.charAt(i) == '*') {
+        sb.setCharAt(i, ' ');
+        int j = 0;
+        while (buckets[j].isEmpty())
+          ++j;
+        sb.setCharAt(buckets[j].remove(buckets[j].size() - 1), ' ');
+      } else {
+        buckets[s.charAt(i) - 'a'].add(i);
+      }
+
+    return sb.toString().replaceAll(" ", "");
+  }
+}
